9facbab2dc2e05cbbccc6eb0c8f05e9630e2793e
bvo <bvo@zehlke.com>
2013-06-11 12:50:47 +0200

    Gitlab RUNNING :)

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 site :opscode
 
 cookbook 'rvm', github: 'fnichol/chef-rvm', branch: 'v0.9.0'
+cookbook 'vagrant-ohai', github: 'tknerr/cookbooks-vagrant-ohai'
 metadata
--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'apt','2.0.0'
 depends          'rvm','0.9.0'
+depends          'mysql'
+depends          'database'
+depends          'vagrant-ohai'
