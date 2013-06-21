726f324e0d1f10171f943038e181210e2a9d7899
tkn@zuehlke.com <tkn@zuehlke.com>
2013-06-21 21:28:45 +0200

    up version in metadata to 0.1.0 for development, prepare CHANGELOG

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bvo@zuehlke.com'
 license          'All rights reserved'
 description      'Installs/Configures gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apt', '2.0.0'
 depends 'aws', '0.101.2'
