510113e022bbd3b92b38896f126e607df4d47db3
Torben <tkn@zuehlke.com>
2013-06-29 15:23:34 +0200

    up version for 0.1.2 tag / release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bvo@zuehlke.com'
 license          'All rights reserved'
 description      'Installs/Configures gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 #
 # direct dependencies
